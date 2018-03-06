cab39a0e8d1165782e09e3d1d1a5aa65523d81d3
Matvey Malik <matikstyle@gmail.com>
2018-03-07 02:37:16 +0300

    init commit

---
+++ Assets/CustomScripts/ChangedColor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangedColor : MonoBehaviour {
+
+	public Material noLooky;
+	public Material eyeSpy;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer> ().material = noLooky;
+
+	}
+
+	public void TotallyWatching(){
+		GetComponent<Renderer> ().material = eyeSpy;
+	}
+
+	public void NoLooking(){
+		GetComponent<Renderer> ().material = noLooky;
+	}
+}
+
---
+++ Assets/CustomScripts/CylinderAhead.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CylinderAhead : MonoBehaviour {
+
+	public Animator Anime;
+
+	// Use this for initialization
+	void Start () {
+		Anime = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Anime.Play ("CylinderGo");
+	}
+}
